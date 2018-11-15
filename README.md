5407d3542ca4e53b9928e843048b08c5d26ee3e1
Glitch (protective-talk) <none>
2018-11-15 17:21:37 +0000

    🌸👖 Checkpoint ./README.md:1601221/445 ./index.html:1601221/53 ./vroom-vroom.js:1601221/1102

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Wayve Perception Demo +
 
-This quick demo expands on [Wayve's new SegNet model](https://wayve.ai/blog/2018/10/8/vision-for-driving-with-deep-learning) by inferring off of Google Street Views.
+This quick demo expands on [Wayve's new SegNet model](https://wayve.ai/blog/2018/10/8/vision-for-driving-with-deep-learning) by inferring off of Google Street Views. You can extend this idea by automating the Street View using only the learned data, collecting it all for use later on in a cloud service while you sleep!
+
+While their demo falls under the Creative Commons code, I've kept the code minified. However, you 
---
+++ vroom-vroom.js
@@ -0,0 +1,32 @@
+// This is the street view object
+const pano = window.pano
+
+window.initialize = function () {
+  const $rawImg = document.querySelector('#image-raw')
+  $rawImg.crossOrigin = ''
+  
+  /**
+   * Setup Google Street View
+   */
+  const pano = window.pano = new google.maps.StreetViewPanorama(document.getElementById('street-view'), {
+    position: {lat: 45.5124601, lng: -122.6618953},
+    pov: {heading: 165, pitch: 0},
+    zoom: 1
+  })
+
+  /**
+   * Set a new image based on the pano change
+   */
+  pano.addListener('pano_changed', function() {inferOnPano()})
+  pano.addListener('position_changed', function() {inferOnPano()})
+  pano.addListener('pov_changed', function() {inferOnPano()})
+
+  /**
+   * Triggers a new inference
+   */
+  const inferOnPano = function () {
+    const pov = pano.getPov()
+    const pos = pano.getPosition()
+    $rawImg.src = `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${pos.lat()},${pos.lng()}&fov=90&heading=${pov.heading}&pitch=${pov.pitch}&key=AIzaSyCr2mKyJiFfXxLE6MYx6b-svg7oQEfw0u4`
+  }
+}
